691321a2b83c6e8c3e4dcebc788010eb5ea8dcf0
Neha C J <119199146+neha-cj@users.noreply.github.com>
2024-05-17 13:09:47 +0530

    Update worstfit.c

--- memAllocat/worstfit.c
+++ memAllocat/worstfit.c
@@ -6,16 +6,16 @@ void worstFit(int blockSize[], int m, int processSize[], int n) {
     memset(allocation, -1, sizeof(allocation));
 
     for (int i = 0; i < n; i++) {
-        int wstIdx = -1;
+        int wst = -1;
         for (int j = 0; j < m; j++) {
             if (blockSize[j] >= processSize[i]) {
-                if (wstIdx == -1 || blockSize[wstIdx] < blockSize[j])
-                    wstIdx = j;
+                if (wst == -1 || blockSize[wst] < blockSize[j])
+                    wst = j;
             }
         }
-        if (wstIdx != -1) {
-            allocation[i] = wstIdx;
-            blockSize[wstIdx] -= processSize[i];
+        if (wst!= -1) {
+            allocation[i] = wst;
+            blockSize[wst] -= processSize[i];
         }
     }
 
@@ -33,10 +33,50 @@ void worstFit(int blockSize[], int m, int processSize[], int n) {
 }
 
 int main() {
-    int blockSize[] = {100, 500, 200, 300, 600};
-    int processSize[] = {212, 417, 112, 426};
-    int m = sizeof(blockSize) / sizeof(blockSize[0]);
-    int n = sizeof(processSize) / sizeof(processSize[0]);
+    printf("Worst Fit Allocation\n");
+    int m,n;
+    printf("enter number of blocks: ");
+    scanf("%d", &m);
+    printf("enter number of process: ");
+    scanf("%d",&n);
+    int blockSize[m], processSize[n];
+
+    printf("Enter the sizes of the blocks:\n");
+    for (int i = 0; i < m; i++) {
+        printf("Block %d: ", i + 1);
+        scanf("%d", &blockSize[i]);
+    }
+    printf("Enter the sizes of the processes:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &processSize[i]);
+    }
     worstFit(blockSize, m, processSize, n);
     return 0;
 }
+
+/*OUPUT
+Worst Fit Allocation
+enter number of blocks: 5
+enter number of process: 4
+Enter the sizes of the blocks:
+Block 1: 100
+Block 2: 500
+Block 3: 200
+Block 4: 300
+Block 5: 600
+Enter the sizes of the processes:
+Process 1: 212
+Process 2: 417
+Process 3: 112
+Process 4: 426
+
+Process No.     Process Size    Block no.
+ 1              212             5
+ 2              417             2
+ 3              112             5
+ 4              426             Not Allocated
+Free Blocks
+100->83->200->300->276->
+
+*/
