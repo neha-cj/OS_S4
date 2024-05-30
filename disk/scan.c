13d15bec4a0df49b41b3f452e2944dce51a9e61d
Neha C J <119199146+neha-cj@users.noreply.github.com>
2024-05-30 10:48:11 +0530

    Create scan.c

---
+++ disk/scan.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main() {
+    int RQ[100],i,j,n,TotalHeadMoment=0,initial,size,move,temp,index;
+    printf("Enter the no of requests: ");
+    scanf("%d",&n);
+    printf("Enter the requests sequence: ");
+    for(i=0;i<n;i++) {
+        scanf("%d",&RQ[i]);
+    }
+    printf("Enter initial head position: ");
+    scanf("%d",&initial);
+    printf("Enter total disk size: ");
+    scanf("%d",&size);
+    printf("Enter the head movement direction(1/0): ");
+    scanf("%d",&move);
+    
+    //Sort the request array
+    for(i=0;i<n;i++) {
+        for(j=0;j<n-i-1;j++) {
+            if(RQ[j]>RQ[j+1]) {
+                temp=RQ[j];
+                RQ[j]=RQ[j+1];
+                RQ[j+1]=temp;
+            }
+        }
+    }
+    
+    for(i=0;i<n;i++) {
+        if(initial<RQ[i]) {
+            index=i;
+            break;
+        }
+    }
+    
+    //if movement is towards high value
+    if(move==1) {
+        for(i=index;i<n;i++) {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+        //last movement for max size
+        TotalHeadMoment=TotalHeadMoment+abs(size-RQ[i-1]-1);
+        initial=size-1;
+        for(i=index-1;i>=0;i--) {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+    }
+    
+    //if movement is towards low value
+    else{
+        for(i=index-1;i>=0;i--) {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+        //last movement for min size
+        TotalHeadMoment=TotalHeadMoment+abs(RQ[i+1]-0);
+        initial=0;
+        for(i=index;i<n;i++) {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+    }
+    
+    printf("Total head movement is %d",TotalHeadMoment);
+}
+
+/*OUTPUT 
+Enter the no of requests: 7
+Enter the requests sequence: 82 170 43 140 24 16 190
+Enter initial head position: 53
+Enter total disk size: 200
+Enter the head movement direction(1/0): 0
+Total head movement is 243
+*/
