ffeaa3c8e79b7f40f4370eac2554f1b91696bbee
Neha C J <119199146+neha-cj@users.noreply.github.com>
2024-07-29 20:14:40 +0530

    Update sequential.c

--- fileallocation/sequential.c
+++ fileallocation/sequential.c
@@ -44,3 +44,79 @@ int main() {
 
     return 0;
 }
+//better method
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+    int n, i, j, b[20], sb[20], mem[100], ch, count=0;
+    for (i=0 ; i<100 ; i++)
+        mem[i]=0;
+    for (i=0 ; i<20 ; i++){
+        b[i]=0;
+        sb[i]=0;
+    }
+    do{
+        count++;
+        printf("\nEnter the number of blocks occupied by file %d: ", count);
+        scanf("%d", &b[count]);
+        int success = 0;
+        while (!success) {
+            success = 1;
+            printf("\nEnter the starting block of file %d: ", count);
+            scanf("%d", &sb[count]);
+            for (j=sb[count] ; j<(sb[count]+b[count]) ; j++){
+                if (mem[j] == 0){
+                    mem[j]=j;
+                    printf("Block %d has been allocated for file %d\n", j, count);
+                }
+                else{
+                    printf("\nBlock %d is already allocated", j);
+                    printf("\nTry again!!\n");
+                    for (int k=sb[count] ; k<j ; k++)
+                        mem[k]=0;
+                    success = 0;
+                    break;
+                }
+            }
+        }
+        printf("\nFile Allocation Successful\n");
+        printf("\nPress 1 to continue allocation, Press 0 to stop allocation: ");
+        scanf("%d", &ch);
+    }
+    while (ch==1);
+    printf("\nFile\tStart\tBlock Length\n");
+    for (j=1 ; j<=count ; j++)
+        printf("%2d %9d %10d\n", j, sb[j], b[j]);
+}
+/*OUTPT
+Enter the number of blocks occupied by file 1: 3
+
+Enter the starting block of file 1: 3
+Block 3 has been allocated for file 1
+Block 4 has been allocated for file 1
+Block 5 has been allocated for file 1
+
+File Allocation Successful
+
+Press 1 to continue allocation, Press 0 to stop allocation: 1
+
+Enter the number of blocks occupied by file 2: 2
+
+Enter the starting block of file 2: 4
+
+Block 4 is already allocated
+Try again!!
+
+Enter the starting block of file 2: 6
+Block 6 has been allocated for file 2
+Block 7 has been allocated for file 2
+
+File Allocation Successful
+
+Press 1 to continue allocation, Press 0 to stop allocation: 0
+
+File    Start   Block Length
+ 1         3          3
+ 2         6          2
+*/
